cc0b83bae26eae5b1495581de8590d14dbf3dbf8
Delio Darwin <deliodarwin@gmail.com>
2024-06-30 21:17:57 -0300

    Remoção da Key

--- RabitMQUsuario.ConsoleApplication/obj/Debug/net6.0/RabitMQUsuarioConsoleApplication.AssemblyInfo.cs
+++ RabitMQUsuario.ConsoleApplication/obj/Debug/net6.0/RabitMQUsuarioConsoleApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RabitMQUsuarioConsoleApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78fa95b34dae77092ca4583e09fd9664d5a72226")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b074c8a1d61bf60cdcc3a80e2f6ecf3f4b65e72")]
 [assembly: System.Reflection.AssemblyProductAttribute("RabitMQUsuarioConsoleApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RabitMQUsuarioConsoleApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
